c831c84199b7b0ffd0bab4913ea4a36a684be941
layelle315 <jackykotsaf@gmail.com>
2025-03-17 11:49:10 +0100

    Ajout des fichiers Dockerfile, app.py, requirements.txt

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Bonjour à tous, Ceci est une simple application conteneurisée avec Docker par mon_nom!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
